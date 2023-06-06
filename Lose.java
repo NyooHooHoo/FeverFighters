dcae3a295de608882f1243d54b95e01c02bb8ccd
laurnrh <laurenhewang727@gmail.com>
2023-06-06 11:10:36 -0400

    changed name of frame

--- Lose.java
+++ Lose.java
@@ -1,29 +1,29 @@
 import javax.swing.JFrame;
 import javax.swing.JComponent;
 import java.awt.*;
-public class Win {
+public class Lose {
     Font titleFont = new Font("Serif", Font.BOLD, 65);
     Font text = new Font("Monospace", Font.BOLD, 25);
 
     Color pink = new Color(255, 200, 221);
-    Color green = new Color(150, 224, 114);
+    Color red = new Color(234, 153, 153);
 
-    public Win(){
-        JFrame frame = new JFrame("Win");
+    public Lose(){
+        JFrame frame = new JFrame("Lose");
         frame.setSize(600, 500);
-        frame.add(new Win.Drawing());
+        frame.add(new Lose.Drawing());
         frame.setVisible(true);
     }
     public static void main(String[] args) {
-        new Win();
+        new Lose();
     }
     class Drawing extends JComponent{
         public void paint(Graphics g){
-            g.setColor(green);
+            g.setColor(red);
             g.fillRect(0, 0, 1200, 800);
             g.setFont(titleFont);
             g.setColor(Color.BLACK);
-            g.drawString("You WIN!", 150, 120);
+            g.drawString("GAME OVER", 150, 120);
 
             int yPos = 210;
             g.setFont(text);
